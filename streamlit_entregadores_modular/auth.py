36f37bbcf936971f908d3d40587920d9df89ecfb
GNJotinha <g.nascimento6868@gmail.com>
2025-09-01 10:19:25 -0300

    Update auth.py

--- streamlit_entregadores_modular/auth.py
+++ streamlit_entregadores_modular/auth.py
@@ -3,14 +3,8 @@ import bcrypt
 
 USUARIOS = st.secrets.get("USUARIOS", {})
 
+import streamlit as st
+USUARIOS = st.secrets.get("USUARIOS", {})
+
 def autenticar(usuario, senha):
-    user = USUARIOS.get(usuario)
-    if not user:
-        return False
-    senha_hash = user.get("senha_hash", "")
-    if not senha_hash:
-        return False
-    try:
-        return bcrypt.checkpw(senha.encode(), senha_hash.encode())
-    except Exception:
-        return False
+    return usuario in USUARIOS and USUARIOS[usuario]["senha"] == senha
